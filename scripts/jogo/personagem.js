a59692c556b4dbfb6bade09565c58cbfbf079137
Rodrigo Souza <ro_web1@hotmail.com>
2020-06-24 19:11:46 -0300

    criando classe para personagem

---
+++ scripts/jogo/personagem.js
@@ -0,0 +1,9 @@
+class Personagem {
+    constructor(imagem) {
+        this.imagem = imagem;
+        this.height = height;
+    }
+    exibe() {
+        image(this.imagem, 0, height - 135, 110, 135, 0, 0, 220, 270);
+    }
+}
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,33 +1,24 @@
 let imagemCenario;
 let imagemPersonagem;
-let cenario;
+let cenarioJogo;
+let personagemJogo;
+let somDoJogo;
 
 function preload() {
   imagemCenario = loadImage('images/cenario/floresta.png');
-  imagemPersonagem = loadImage('images/personagem/correndo.png');
+  personagemJogo = loadImage('images/personagem/correndo.png');
+  somDoJogo = loadSound('sons/trilha_jogo.mp3');
 }
 
 function setup() {
-  cenario = new Cenario(imagemCenario, 50);
   createCanvas(windowWidth, windowHeight);
+  cenarioJogo = new Cenario(imagemCenario, 3);
+  personagemJogo = new Personagem(imagemPersonagem);
+  somDoJogo.loop();
 }
 
 function draw() {
-  cenario.exibe();
-  image(imagemPersonagem, 0, height - 135, 110, 135, 0, 0, 220, 270);
-}
-
-class Cenario {
-  constructor(imagem, velocidade) {
-    this.imagem = imagem;
-    this.velocidade = velocidade;
-  }
-
-  exibe() {
-    image(this.imagem, -this.velocidade, 0, width, height);
-    image(this.imagem, width, this.velocidade - 1, width, height);
-  }
-
-  move() {
-  }
+  cenarioJogo.exibe();
+  cenarioJogo.move();
+  personagemJogo.exibe();
 }
